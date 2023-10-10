08e6733120a2a4480e91a77709b6c00c2f192043
Carl J du Preez <carljdp@gmail.com>
2023-10-10 14:21:01 +0200

    Basic form (working)

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,125 @@
 import 'package:flutter/material.dart';
-
-const String title = 'Form';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp(title: title));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.title});
-  final String title;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(title: title),
+    return const MaterialApp(
+      title: 'MyForm',
+      home: MyForm(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  MyFormState createState() => MyFormState();
+}
+
+class MyFormState extends State<MyForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _emailFocus = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 3000), () {
+      FocusScope.of(context).requestFocus(_nameFocus);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You are here!',
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('MyForm')),
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  focusNode: _nameFocus,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_emailFocus);
+                  },
+                  // autofocus: true, // Add this line
+                ),
+                TextFormField(
+                  focusNode: _emailFocus,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.done,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).unfocus();
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0), // Add top padding
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          SystemNavigator.pop(); // Exit the app
+                        },
+                        child: const Text('Cancel'),
+                      ),
+                      const SizedBox(
+                          width: 16), // Optional spacing between the buttons
+                      ElevatedButton(
+                        onPressed: () {
+                          // Show submitted info and exit or restart app
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Info submitted'),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                            Future.delayed(const Duration(seconds: 3), () {
+                              if (const bool.fromEnvironment(
+                                  "dart.vm.product")) {
+                                // Exit the app if in release mode
+                                // TODO: Add your exit code here
+                              } else {
+                                // Restart the app if in debug mode
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const MyApp()),
+                                );
+                              }
+                            });
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
